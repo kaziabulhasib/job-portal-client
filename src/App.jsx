2f2a265dbd6c5274450160dfdc9ececda7f37594
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-11 22:57:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <h1 className='text-red-500 text-3xl font-semibold'>dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
