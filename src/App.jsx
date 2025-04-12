642e4a32642c0360fa7d62558a4676300cde0880
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-12 19:55:28 +0530

    route setup & daisy setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='text-red-500 text-3xl font-semibold'>dashboard</h1>
+      {/* <button className='btn btn-primary'>click</button> */}
     </div>
   );
 }
