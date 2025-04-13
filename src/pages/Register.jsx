4c8940e9a8092d7074ad05cfe45afe51052db5d7
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-13 22:41:46 +0530

    form submit with vlidation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,25 +3,55 @@ import lottieAnimation from "../assets/registeranimation.json";
 import Lottie from "lottie-react";
 
 function Register() {
+  const [passwordError, setPasswordError] = React.useState("");
+  
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setPasswordError(
+        "Password must be at least 6 characters and contain one uppercase, one lowercase, one number, and one special character"
+      );
+      return;
+    }
+    setPasswordError("");
+    console.log(email, password);
+    form.reset();
+  };
   return (
     <div className='hero bg-base-200 min-h-screen'>
       <div className='hero-content flex-col lg:flex-row-reverse'>
-        <div className='text-center lg:text-left'>
+        <div className='text-center lg:text-left w-[40%] md:w-[60%]'>
           <Lottie animationData={lottieAnimation} loop={true} />
         </div>
         <div className='card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl'>
           <div className='card-body'>
             <h1 className='text-4xl font-bold ml-2 mt-4'>Login now!</h1>
-            <fieldset className='fieldset'>
+            <form onSubmit={handleRegister} className='fieldset'>
               <label className='fieldset-label'>Email</label>
-              <input type='email' className='input' placeholder='Email' />
+              <input
+                type='email'
+                name='email'
+                className='input'
+                placeholder='Email'
+              />
               <label className='fieldset-label'>Password</label>
-              <input type='password' className='input' placeholder='Password' />
+              <input
+                type='password'
+                name='password'
+                className='input'
+                placeholder='Password'
+              />
+              {passwordError && <p className='text-red-500'>{passwordError}</p>}
               <div>
                 <a className='link link-hover'>Forgot password?</a>
               </div>
               <button className='btn btn-neutral mt-4'>Login</button>
-            </fieldset>
+            </form>
           </div>
         </div>
       </div>
