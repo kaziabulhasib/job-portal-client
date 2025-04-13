324259d0ba72c39bb5cccf16b787a3971d1a58b1
kaziabulhasib <hasibkazi420@gmail.com>
2025-04-13 22:13:20 +0530

    adding lootie animation to register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,17 @@
 import React from "react";
+import lottieAnimation from "../assets/registeranimation.json";
+import Lottie from "lottie-react";
 
 function Register() {
   return (
     <div className='hero bg-base-200 min-h-screen'>
       <div className='hero-content flex-col lg:flex-row-reverse'>
         <div className='text-center lg:text-left'>
-          
-          <p className='py-6'>
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <Lottie animationData={lottieAnimation} loop={true} />
         </div>
         <div className='card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl'>
           <div className='card-body'>
+            <h1 className='text-4xl font-bold ml-2 mt-4'>Login now!</h1>
             <fieldset className='fieldset'>
               <label className='fieldset-label'>Email</label>
               <input type='email' className='input' placeholder='Email' />
